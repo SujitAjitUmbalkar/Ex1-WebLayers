218e3c8e094963f631ef4158bd924c1876a657e6
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-17 22:00:36 +0530

    Added functionality for time, @JsonFormat

--- weblayers/src/main/java/com/weblayerexample/weblayers/advice/ApiResponse.java
+++ weblayers/src/main/java/com/weblayerexample/weblayers/advice/ApiResponse.java
@@ -1,12 +1,16 @@
 package com.weblayerexample.weblayers.advice;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@JsonPropertyOrder({ "timeStamp", "data", "error" })
 public class ApiResponse<T>
 {
+    @JsonFormat(pattern = "hh:mm:ss dd - MM - yyyy")
     private LocalDateTime timeStamp;
 
     private T data;
