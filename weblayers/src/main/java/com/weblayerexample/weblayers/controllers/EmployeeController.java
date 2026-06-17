73b88ad8227ff528f3db69dd8ee0e1fcbe5d1df5
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-17 14:31:13 +0530

    Added Controller level Exception

--- weblayers/src/main/java/com/weblayerexample/weblayers/controllers/EmployeeController.java
+++ weblayers/src/main/java/com/weblayerexample/weblayers/controllers/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/employees")
@@ -19,60 +20,52 @@ public class EmployeeController
     private final EmployeeService employeeService;
 
     @GetMapping("/{employeeId}")
-    public ResponseEntity<EmployeeDto> getEmployeeById(
-            @PathVariable Long employeeId)
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long employeeId)
     {
         return employeeService.getEmployeeById(employeeId)
                 .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+                .orElseThrow(()-> new NoSuchElementException("employee not found "));
     }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> HandleException(NoSuchElementException e)
+    {
+        return new  ResponseEntity<>("Employee not found", HttpStatus.NOT_FOUND);
+    }
+//Exception handling is not mainly for preventing crashes.
+//It is for:
+//Converting Java exceptions into proper HTTP responses.
 
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(
             @RequestParam(required = false, name = "InputAge") Integer age,
             @RequestParam(required = false) String sortBy)
     {
-        return ResponseEntity.ok(
-                employeeService.getAllEmployees(age, sortBy)
-        );
+        return ResponseEntity.ok(employeeService.getAllEmployees(age, sortBy));
     }
 
     @PostMapping
-    public ResponseEntity<EmployeeDto> createNewEmployee(
-           @Valid @RequestBody EmployeeDto inputEmployee)
+    public ResponseEntity<EmployeeDto> createNewEmployee(@Valid @RequestBody EmployeeDto inputEmployee)
     {
-        EmployeeDto savedEmployee =
-                employeeService.createNewEmployee(inputEmployee);
+        EmployeeDto savedEmployee = employeeService.createNewEmployee(inputEmployee);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedEmployee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployee);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EmployeeDto> updateEmployeeById(
-            @Valid @RequestBody EmployeeDto inputEmployee,
-            @PathVariable Long id)
+    public ResponseEntity<EmployeeDto> updateEmployeeById(@Valid @RequestBody EmployeeDto inputEmployee, @PathVariable Long id)
     {
-        return ResponseEntity.ok(
-                employeeService.updateEmployee(inputEmployee, id)
-        );
+        return ResponseEntity.ok(employeeService.updateEmployee(inputEmployee, id));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<EmployeeDto> patchEmployeeById(
-            @PathVariable Long id,
-            @Valid @RequestBody Map<String, Object> update)
+    public ResponseEntity<EmployeeDto> patchEmployeeById(@PathVariable Long id, @Valid @RequestBody Map<String, Object> update)
     {
-        return ResponseEntity.ok(
-                employeeService.patchEmployeeById(id, update)
-        );
+        return ResponseEntity.ok(employeeService.patchEmployeeById(id, update));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEmployeeById
-            (
-                @PathVariable Long id
-            )
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable Long id)
     {
         employeeService.deleteEmployeeById(id);
 
